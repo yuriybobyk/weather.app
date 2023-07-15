20f987c69b07949324d02e8a093d8eca8be3f980
yuriybobyk <yuriybobyk4325@gmail.com>
2023-07-15 16:43:34 +0300

    Forecast done

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -60,7 +60,7 @@ const Search = () => {
         <main
             className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
                 <section
-                    className="w-full md:max-w-[650px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[650px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+                    className="w-full md:max-w-[850px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[650px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
                     <h1 className="text-4xl font-thin">Weather <span className="font-black">Forecast</span></h1>
                     <p className="text-sm mt-2">Enter below a place you want to know the weather of and select an option
                         from the dropdown</p>
